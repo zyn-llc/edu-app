9fea02a94c2bfe34c59f9d3a68895c099525a310
zyn-llc <vitamin.z.community@gmail.com>
2026-08-17 04:33:08 -0700

    Update app_settings.dart

--- mobile/lib/core/app_settings.dart
+++ mobile/lib/core/app_settings.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// Light/dark/system. Defaults to following the device.
 final themeModeProvider = StateProvider<ThemeMode>((_) => ThemeMode.system);
 
-/// Whether reward/error sounds play. User-toggleable in Settings.
+
 final soundEnabledProvider = StateProvider<bool>((_) => true);
